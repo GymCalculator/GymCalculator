d00b1942074fe6cd4b1a2e3cbd54e3b895f658b5
MarkoZoreski <83420035+MarkoZoreski@users.noreply.github.com>
2022-04-19 17:32:34 +0200

    small logic change

--- GymCalculator_2/src/main/java/com/example/gymcalculator_2/repository/UserRepository.java
+++ GymCalculator_2/src/main/java/com/example/gymcalculator_2/repository/UserRepository.java
@@ -5,10 +5,11 @@ import com.example.gymcalculator_2.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User,String> {
 
-//    public List<User> findUsersByUsername(List<String> username); // one
-//    public List<User> findAllByUsername (List<String> username); // or the other -> idk both might be OK
-
+    Optional<User> findByUsernameAndPassword(String username, String password);
+    Optional<User> findByUsername(String username);
 }
--- GymCalculator_2/src/main/java/com/example/gymcalculator_2/service/impl/UserServiceImpl.java
+++ GymCalculator_2/src/main/java/com/example/gymcalculator_2/service/impl/UserServiceImpl.java
@@ -2,31 +2,44 @@ package com.example.gymcalculator_2.service.impl;
 
 
 import com.example.gymcalculator_2.model.User;
+import com.example.gymcalculator_2.repository.LoggedLiftsRepository;
 import com.example.gymcalculator_2.repository.UserRepository;
 import com.example.gymcalculator_2.service.UserService;
+import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.stereotype.Service;
 
+import java.nio.file.attribute.UserPrincipal;
+import java.nio.file.attribute.UserPrincipalNotFoundException;
 import java.util.List;
 
 @Service
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
+    private final LoggedLiftsRepository loggedLiftsRepository;
+    //private final PasswordEncoder passwordEncoder;
 
-    public UserServiceImpl(UserRepository userRepository) {
+    public UserServiceImpl(UserRepository userRepository, LoggedLiftsRepository loggedLiftsRepository) {
         this.userRepository = userRepository;
+        //this.passwordEncoder = passwordEncoder;
+        this.loggedLiftsRepository = loggedLiftsRepository;
     }
+
+//    @Override
+//    public User register(String username, String password) {
+//        String encryptedPassword = this.passwordEncoder.encode(password);
+//        User user = new User(username,password);
+//        return this.userRepository.save(user);
+//    }
 
     @Override
     public List<User> listUsers() {
         return userRepository.findAll();
     }
 
-
-//    @Override
-//    public Optional<List<User>> findAllFriends(List<String> friendUsernames) {
-//        return Optional.of( userRepository.findAllByUsername(friendUsernames) );
-//    }
-
-
+    @Override
+    public List<String> findFriends(User user) throws ChangeSetPersister.NotFoundException {
+        User currentUser = userRepository.findByUsername(user.getUsername()).orElseThrow(ChangeSetPersister.NotFoundException::new);
+        return currentUser.getFriends();
+    }
 
 }
